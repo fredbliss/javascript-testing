a5d727a596ee0da70b973100d7452fcd06e5efca
Fred Bliss <fred@cpxsecurity.com>
2020-01-24 14:01:49 -0500

    snapshots

---
+++ src/shared/calculator-display.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import AutoScalingText from './auto-scaling-text'
+import {getFormattedValue} from './utils'
+
+function CalculatorDisplay({value, ...props}) {
+  const formattedValue = getFormattedValue(
+    value,
+    typeof window === 'undefined' ? 'en-US' : window.navigator.language,
+  )
+
+  return (
+    <div
+      {...props}
+      id="calculator-display"
+      css={{
+        position: 'relative',
+        color: 'white',
+        background: '#1c191c',
+        lineHeight: '130px',
+        fontSize: '6em',
+        flex: '1',
+      }}
+    >
+      <AutoScalingText>{formattedValue}</AutoScalingText>
+    </div>
+  )
+}
+
+CalculatorDisplay.propTypes = {
+  value: PropTypes.string.isRequired,
+}
+
+export default CalculatorDisplay
