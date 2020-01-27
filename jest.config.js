ecdf5232868b827593817fdc91618fe5f24813dd
Fred Bliss <fred@cpxsecurity.com>
2020-01-27 16:40:09 -0500

    more comments

--- jest.config.js
+++ jest.config.js
@@ -2,12 +2,15 @@ module.exports = {
   ...require('./test/jest-common'),
   collectCoverageFrom: ['**/src/**/*.js'],
   coverageThreshold: {
+    //whatever pass/fail constraints we want to enforce coverage-wise
     global: {
       statements: 15,
       branches: 10,
       functions: 15,
       lines: 15,
     },
+    //whatever pass/fail constraints we want to enforce coverage-wise, file-specific
+    //this is great if some files are very critical in our app and need to be as tested as possible.
     './src/shared/utils.js': {
       statements: 100,
       branches: 80,
