395ac0e0461798e556aec1b5bb67006cebc161dd
Fred Bliss <fred@cpxsecurity.com>
2020-01-27 11:55:42 -0500

    overload render with our own including providers (theme, redux, router, etc.)

--- src/shared/__tests__/calculator-display.js
+++ src/shared/__tests__/calculator-display.js
@@ -1,30 +1,33 @@
 import React from 'react'
-import {render} from '@testing-library/react'
+import {render} from '../../../test/calculator-test-utils'
 import CalculatorDisplay from '../calculator-display'
 
 test('renders', () => {
-  const {container} = render(<CalculatorDisplay value="0" />)
+  const {container, rerender} = render(<CalculatorDisplay value="0" />)
+
+  rerender(<CalculatorDisplay value="1" />)
+
   expect(container.firstChild).toMatchInlineSnapshot(`
     .emotion-0 {
       position: relative;
-      color: white;
-      background: #1c191c;
       line-height: 130px;
       font-size: 6em;
       -webkit-flex: 1;
       -ms-flex: 1;
       flex: 1;
+      color: white;
+      background: #1c191c;
     }
 
     <div
-      class="emotion-0"
+      class="emotion-0 emotion-1"
     >
       <div
         class="autoScalingText"
         data-testid="total"
         style="transform: scale(1,1);"
       >
-        0
+        1
       </div>
     </div>
   `)
