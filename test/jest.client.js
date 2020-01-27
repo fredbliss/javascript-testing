42f741f960002603f559a00f473d395ddb763d28
Fred Bliss <fred@cpxsecurity.com>
2020-01-27 16:31:58 -0500

    simplified the client/server setup for jest

--- test/jest.client.js
+++ test/jest.client.js
@@ -3,18 +3,4 @@ module.exports = {
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   snapshotSerializers: ['jest-emotion'],
-  coverageThreshold: {
-    global: {
-      statements: 15,
-      branches: 10,
-      functions: 15,
-      lines: 15,
-    },
-    './src/shared/utils.js': {
-      statements: 100,
-      branches: 80,
-      functions: 100,
-      lines: 100,
-    },
-  },
 }
